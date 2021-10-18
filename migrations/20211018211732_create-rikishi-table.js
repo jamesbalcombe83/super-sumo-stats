503a7444561d31d2511bab6ec6acc49a1475ce62
jamesbalcombe83 <james.balcombe83@gmail.com>
2021-10-19 06:27:03 +0900

    Feat: added scrape files

--- migrations/20211018211732_create-rikishi-table.js
+++ migrations/20211018211732_create-rikishi-table.js
@@ -1,8 +1,20 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('rikishi', (table) => {
+        table.increments('id').primary()
+        table.string('ringName')
+        table.string('realName')
+        table.string('birthPlace')
+        table.date('dateOfBirth')
+        table.string('heya')
+        table.string('highestRank')
+        table.decimal('height',4,1)
+        table.decimal('weight',4,1)
+        table.date('openingBasho')
+        table.date('retirementBasho')
+      })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable("locations");
 };
