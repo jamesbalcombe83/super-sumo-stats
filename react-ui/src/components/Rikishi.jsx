eb2bdb4b686dd9b7e5a49766e3983a86e166a2d4
james.balcombe83 <james.balcombe83@gmail.com>
2021-10-27 07:09:25 +0900

    Fix: incomplete no id on render issue

--- react-ui/src/components/Rikishi.jsx
+++ react-ui/src/components/Rikishi.jsx
@@ -9,28 +9,34 @@ export default function Rikishi(props) {
     //import a state from the store and use anywhere with useRecoilValue and the atom name
     const [rikishi, setRikishi] = useRecoilState(rikishiState); 
 
-    let id;
+    let id, display;
     //select the correct id based on the rikishi component displaying the data
     if (props.id ==='riki1') {
         id = Number(rikishi[0].id);
     } else {
         id = Number(rikishi[1].id);
     }
-    const { data , loading, error } = useAxios({
-        url: `/rikishi?id=${id}`
-    });
 
-    if (loading || !data) return "Select a Rikishi";
-    if (error) return "Error";
+    if (id) {
+        const { data , loading, error } = useAxios({
+            url: `/rikishi?id=${id}`
+        });
+        if (loading || !data) return "Select a Rikishi";
+        if (error) return "Error";
+        display = (
+            <div>
+                <p>Shikona: {data.ringName}</p>
+                <p>Birth place: {data.birthPlace}</p>
+                <p>Heya: {data.heya}</p>
+                <p>Highest Rank: {data.highestRank}</p>
+                <p>Weight: {data.weight} kg</p>
+                <p>Height: {data.height} m</p> 
+            </div>  
+        )
+    }
 
     return (
-        <div>
-          <p>Shikona: {data.ringName}</p>
-          <p>Birth place: {data.birthPlace}</p>
-          <p>Heya: {data.heya}</p>
-          <p>Highest Rank: {data.highestRank}</p>
-          <p>Weight: {data.weight} kg</p>
-          <p>Height: {data.height} m</p> 
-        </div>
+       !id ? <p>Select a Rikishi</p> : 
+        display
     )
 }
